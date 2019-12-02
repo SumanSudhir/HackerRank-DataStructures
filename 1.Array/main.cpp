288c1a45117ebf3bc759197244bdd7cb1aa1caee
SumanSudhir <sudhiriitb27@gmail.com>
2019-12-02 20:46:06 +0530

    1 probelms done

---
+++ 1.Array/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  std::vector<int> arr(n);
+  for(int i = 0; i < n; i++){
+      cin >> arr[i];
+  }
+
+  for(int j = n-1; j >= 0; j--){
+    cout << arr[j] << " ";
+  }
+  return 0;
+}
--- 2.2DArray/main.cpp
+++ 2.2DArray/main.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int hourglassSum(vector<vector<int>>arr){
-    
+  int max = 8;
+  int temp = 8;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
 
+            temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            if(i == 0 && j == 0){
+                max = temp;
+            }
+
+            if(temp > max){
+                max = temp;
+            }
+        }
+    }
+    return max;
 }
 
 int main(){
