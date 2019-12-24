2474b3485a2781bc50697623ed37cfaa96e8c629
SumanSudhir <sudhiriitb27@gmail.com>
2019-12-25 01:36:45 +0530

    tree formed

--- 031.SwapNodes/main.cpp
+++ 031.SwapNodes/main.cpp
@@ -1,16 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inOrder(Node *root) {
+    if(root == NULL){
+        return;
+    }
+
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+
+}
+
 int main(){
     //Taking Input
     int nodes;
     cin>>nodes;
 
     vector<vector<int>>index(nodes);
-
+    queue<Node*>q;
+    Node* root = new Node(1);
+    q.push(root);
     for(int i=0;i<nodes;i++){
         index[i].resize(2);
         cin>>index[i][0]>>index[i][1];
+        Node* temp = q.front();
+        if(index[i][0] != -1){
+            Node* new_nodel = new Node(index[i][0]);
+            temp->left = new_nodel;
+            q.push(new_nodel);
+        }
+            if(index[i][1] != -1){
+            Node* new_noder = new Node(index[i][1]);
+            temp->right = new_noder;
+            q.push(new_noder);
+        }
+        q.pop();
     }
 
     int n_query;
@@ -21,12 +57,6 @@ int main(){
         cin>>query[i];
     }
 
-    
-
-
-
-    // for(int i=0;i<nodes;i++){
-    //     cout<<index[i][0]<<" "<<index[i][1]<<endl;
-    // }
+    inOrder(root);
 
 }
